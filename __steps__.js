6f78c934a2514f478a00f6e307879b44fefbb636
Ruhul Amin <ruhulaminbabu93@gmail.com>
2024-12-02 13:21:02 +0600

    simple api build with express

---
+++ __steps__.js
@@ -0,0 +1,9 @@
+/**
+ *  1. create a post api on the server side
+ *  2. send data via post
+ *  3. set fetch method inside the fetch options (second perameter)
+ *  4. options will have three things: method
+ *  5. option will have header: 'content-type': 'application/json'
+ *  6. don't forget to send data by JSON.STringify in the body:
+ *  7. on the server side: don't forget to use express.json() midleware
+ */
